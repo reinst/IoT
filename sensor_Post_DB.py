058f5eaafbc47249e29441a9453c7c3abd1f435b
Reinst <reinst@users.noreply.github.com>
2019-06-19 21:32:48 -0500

    Update sensor_Post_DB.py

--- sensor_Post_DB.py
+++ sensor_Post_DB.py
@@ -1,59 +1,45 @@
 import socket
+import dht # sensor 
 from machine import Pin
 from time import sleep
-import dht 
 
-## Get sensor readings ##
-
-sensor = dht.DHT22(Pin(15))
-sensor.measure()
-temp = sensor.temperature()
-hum = sensor.humidity()
-temp_f = temp * (9/5) + 32.0
+sensor = dht.DHT22(Pin(16))
 
 #### Send sensor data ####
-    
-host = <YOUR SERVER> ## <-- UPDATE - myserver.com ##
-port = <PORT> ## <-- UPDATE ## - 80/443/3306 or any other
-postDataHere = <PATH TO DB FOR SENSOR DATA>  ## <-- UPDATE - \mysensorDB.php ##
-key = <API KEY FOR AUTH> ## <-- UPDATE - 9834hkgh89t79ohgn ##
-sensorModel = "DHT22"
-location = "Lab0024"
-hum = hum
-temp_f = temp_f
-baseline = "None"
-
-headers = """\
-POST {sensor_url_path} HTTP/1.1\r
-Content-Type: {content_type}\r
-Content-Length: {content_length}\r
-Host: {host}\r
-Connection: close\r
-\r\n"""
-
-
-body = "api_key={key}&sensor={sensorModel}&location={location}&value1={sensor1}&value2={sensor2}&value3={sensor3}"
-
-body_bytes = body.format(
-    key=key,
-    sensorModel=sensorModel,
-    location=location,
-    sensor1=hum,
-    sensor2=temp_f,
-    sensor3=baseline
-    ).encode('UTF-8')
-
-header_bytes = headers.format(
-    sensor_url_path=postDataHere,
-    content_type="application/x-www-form-urlencoded",
-    content_length=len(body_bytes),
-    host=str(host) + ":" + str(port)
-    ).encode('UTF-8')
-
-payload = header_bytes + body_bytes
-
 while True:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
-    s.sendall(payload)
-    sleep(900)
+    print("Measuring.")
+    retry = 0
+    while retry < 3:
+        try:
+            sensor.measure()
+            temp = sensor.temperature()
+            hum = sensor.humidity()
+            temp_f = temp * (9/5) + 32.0
+            break
+        except:
+            retry = retry + 1
+    if retry < 3:
+        print(temp_f)
+        print(hum)
+        host = "127.zerodotzero.one"
+        port = 80
+        postDataHere = "/post_sensor_data.php"
+        key = "tPmAT5Ab3j7F9"
+        sensorModel = "DHT22"
+        location = "Lab0024"
+        humidity = hum
+        tempInF = temp_f
+        baseline = "0.0"
+
+        headers = "POST {sensor_url_path} HTTP/1.1\r\nContent-Type: {content_type}\r\nContent-Length: {content_length}\r\nHost: {host}\r\nConnection: close\r\n\r\n"
+
+        body = "api_key={key}&sensor={sensorModel}&location={location}&value1={sensor1}&value2={sensor2}&value3={sensor3}"
+
+        body_bytes = body.format(key=key,sensorModel=sensorModel,location=location,sensor1=humidity,sensor2=tempInF,sensor3=baseline).encode('UTF-8')
+        header_bytes = headers.format(sensor_url_path=postDataHere,content_type="application/x-www-form-urlencoded",content_length=len(body_bytes),host=str(host) +":"+str(port)).encode('UTF-8')
+        payload = header_bytes + body_bytes
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        s.sendall(payload)
+        print(payload)
+        sleep(5)
